ebc31fc7dc0d85140519b5641e8fb09487ebd79a
Inerf <inerf@mail.ru>
2020-01-09 14:38:49 +0300

    Intersection и Union

--- WindowsFormsRays/SceneObjects/FunctionalAPI.cs
+++ WindowsFormsRays/SceneObjects/FunctionalAPI.cs
@@ -14,9 +14,19 @@ namespace WindowsFormsRays.SceneObjects
             return new RepetitionOp { X = period, Object = figure };
         }
 
+        public static IFigure3D Union(this IFigure3D figure, IFigure3D figure2)
+        {
+            return new UnionOp { Object1 = figure, Object2 = figure2 };
+        }
+
         public static IFigure3D Substract(this IFigure3D figure, IFigure3D figure2)
         {
-            return new SubstractionOp { Object2 = figure, Object1 = figure2 };
+            return new SubstractionOp { Object1 = figure, Object2 = figure2 };
+        }
+
+        public static IFigure3D Intersection(this IFigure3D figure, IFigure3D figure2)
+        {
+            return new IntersectionOp { Object1 = figure, Object2 = figure2 };
         }
     }
 }
